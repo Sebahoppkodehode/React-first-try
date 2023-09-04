6b7d8fa8b4bfa2b2dc030e452df0d18cb62a1dbf
sebahopp <sebahopp.kodehode@gmail.com>
2023-09-05 00:13:29 +0200

    commit

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,9 +1,16 @@
-import { Infocard } from "/assets/components/App.jsx";
+import "./App.css";
+import Header from "./components/Header";
+import MainContent from "./components/MainContent";
+import Footer from "./components/Footer";
 
-{
- <LeopardGecko>
-  <GeckoImage src="assets/pictures/leo.jpg"></GeckoImage>
-  <GeckoName name="Leopard Gecko"></GeckoName>
-  <GeckoDetails details="Leopard geckos are nocturnal, ground-dwelling geckos that can be found in desert environments in Afghanistan, Iran, Pakistan and parts of India. They are generally docile and easy to tame. They do not have the sticky toe pads like other geckos, so they do not climb walls, but unlike other geckos, they do have eyelids."></LeoGeckoDetails>
-</>
+function App() {
+  return (
+    <>
+      <Header />
+      <MainContent />
+      <Footer />
+    </>
+  );
 }
+
+export default App;
